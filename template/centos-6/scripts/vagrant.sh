71c559a6d20b3b4945ae9c1086eecd0e5a2cc31e
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2015-08-14 17:05:56 +0900

    Move vagrant home to /srv/vagrant from /home/vagrant

--- template/centos-6/scripts/vagrant.sh
+++ template/centos-6/scripts/vagrant.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+set -u
+umask 0022
+
 LC_TIME=C date >/etc/vagrant_box_build_time
 
+vagrant_home="/srv/vagrant"
+
 groupadd --system vagrant
-useradd --system -g vagrant -d /srv/vagrant -s /bin/bash -m vagrant
+useradd --system -g vagrant -d "$vagrant_home" -s /bin/bash -m vagrant
 
-echo "umask 022" >>/srv/vagrant/.bashrc
-echo "Defaults:vagrant !requiretty" >/etc/sudoers.d/vagrant
-echo "vagrant ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers.d/vagrant
+echo 'umask 0022' >>"$vagrant_home/.bashrc"
+echo 'Defaults:vagrant !requiretty' >/etc/sudoers.d/vagrant
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers.d/vagrant
 
-mkdir -p ~vagrant/.ssh
+mkdir -p "$vagrant_home/.ssh"
 curl \
   --insecure \
   --location \
-  --output ~vagrant/.ssh/authorized_keys \
+  --output "$vagrant_home/.ssh/authorized_keys" \
   https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub \
 ;
-chown -hR vagrant: ~vagrant/.ssh
+chown -hR vagrant: "$vagrant_home/.ssh"
 
