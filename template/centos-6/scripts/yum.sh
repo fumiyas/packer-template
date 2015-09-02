f7f1f590f9427ba0290fc166ba1a635fa0aaab70
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2015-09-02 16:19:30 +0900

    Add http://archive.kernel.org/centos/ to baseurl

--- template/centos-6/scripts/yum.sh
+++ template/centos-6/scripts/yum.sh
@@ -3,6 +3,9 @@
 set -u
 umask 0022
 
+baseurl="http://vault.centos.org/\$releasever/os/\$basearch/\n\
+  http://archive.kernel.org/centos/\$releasever/os/\$basearch/"
+
 mkdir -p /etc/yum/vars
 sed 's/ *(.*//;s/.* //' /etc/redhat-release >/etc/yum/vars/releasever
 
@@ -11,7 +14,7 @@ cp -a /etc/yum.repos.d/*.repo /etc/yum.repos.d/dist
 for f in /etc/yum.repos.d/*.repo; do
   sed -i \
     -e 's!^mirrorlist=!#&!' \
-    -e 's!^#*baseurl=.*!baseurl=http://archive.kernel.org/centos/$releasever/os/$basearch/!' \
+    -e "s!^#*\(baseurl=\).*!\1$baseurl!" \
     "$f" \
   ;
 done
