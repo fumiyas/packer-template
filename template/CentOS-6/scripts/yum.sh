7313befd05549ff21c19c46fdd45212a64629759
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2016-01-26 17:42:12 +0900

    CentOS: yum.sh: Rewrite CentOS-Base.repo only

--- template/CentOS-6/scripts/yum.sh
+++ template/CentOS-6/scripts/yum.sh
@@ -8,16 +8,14 @@ baseurl="\n\
   http://vault.centos.org/\$releasever/os/\$basearch/\n\
   http://archive.kernel.org/centos-vault/\$releasever/os/\$basearch/"
 
-mkdir -p /etc/yum/vars
-sed 's/ *(.*//;s/.* //' /etc/redhat-release >/etc/yum/vars/releasever
-
 mkdir -p /etc/yum.repos.d/dist
 cp -a /etc/yum.repos.d/*.repo /etc/yum.repos.d/dist
-for f in /etc/yum.repos.d/*.repo; do
-  sed -i \
-    -e 's!^mirrorlist=!#&!' \
-    -e "s!^#*\(baseurl=\).*!\1$baseurl!" \
-    "$f" \
-  ;
-done
+sed -i \
+  -e 's!^mirrorlist=!#&!' \
+  -e "s!^#*\(baseurl=\).*!\1$baseurl!" \
+  /etc/yum.repos.d/CentOS-Base.repo \
+;
+
+mkdir -p /etc/yum/vars
+sed 's/ *(.*//;s/.* //' /etc/redhat-release >/etc/yum/vars/releasever
 
