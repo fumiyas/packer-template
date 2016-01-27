11c9067e0d9dd531ae3db9fc5784cf823e1b018b
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2016-01-28 01:58:50 +0900

    CentOS: info.sh: Log more system information

--- template/CentOS-6/scripts/info.sh
+++ template/CentOS-6/scripts/info.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+set -x
+
+uname -a
+lscpu
+free
+lsblk --all --output name,kname,type,fstype,size,sched,uuid,ro,mountpoint
+cat /etc/fstab
+df -h
+
 rpm -qa
 chkconfig --list
+
+sestatus
+
+ip addr
+ip route
+ss -aeiomp
+iptables -nvL -t filter
+iptables -nvL -t nat
+iptables -nvL -t mangle
+iptables -nvL -t raw
+iptables -nvL -t security
+ip6tables -nvL -t filter
+ip6tables -nvL -t nat
+ip6tables -nvL -t mangle
+ip6tables -nvL -t raw
+ip6tables -nvL -t security
 
