4cfa5d80648ec881259b8515bc5f9ae936f3814c
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2016-01-28 18:53:11 +0900

    CentOS: yum.sh: Fix baseurl for updates, extras and centospuls packages

--- template/CentOS-7/scripts/yum.sh
+++ template/CentOS-7/scripts/yum.sh
@@ -7,31 +7,32 @@ el_ver=$(sed 's/ *(.*//;s/.* //' /etc/redhat-release)
 el_ver_major="${el_ver%%.*}"
 
 latest_baseurl="\n\
-  http://ftp.iij.ad.jp/pub/linux/centos/$el_ver_major/os/\$basearch/\n\
-  http://ftp.kddlabs.co.jp/Linux/packages/CentOS/$el_ver_major/os/\$basearch/\n\
-  http://ftp.jaist.ac.jp/pub/Linux/CentOS/$el_ver_major/os/\$basearch/"
+  http://ftp.iij.ad.jp/pub/linux/centos/$el_ver_major/\\2/\$basearch/\n\
+  http://ftp.kddlabs.co.jp/Linux/packages/CentOS/$el_ver_major/\\2/\$basearch/\n\
+  http://ftp.jaist.ac.jp/pub/Linux/CentOS/$el_ver_major/\\2/\$basearch/"
 vault_baseurl="\n\
-  http://centos.data-hotel.net/pub/linux/centos/\$releasever/os/\$basearch/\n\
-  http://vault.centos.org/\$releasever/os/\$basearch/\n\
-  http://archive.kernel.org/centos-vault/\$releasever/os/\$basearch/"
+  http://centos.data-hotel.net/pub/linux/centos/\$releasever/\\2/\$basearch/\n\
+  http://vault.centos.org/\$releasever/\\2/\$basearch/\n\
+  http://archive.kernel.org/centos-vault/\$releasever/\\2/\$basearch/"
 
 mkdir -p /etc/yum.repos.d/dist
 cp -a /etc/yum.repos.d/CentOS-*.repo /etc/yum.repos.d/dist
+
+sed \
+  -e 's!^mirrorlist=!#&!' \
+  -e "s!^#*\(baseurl=\).*/\([a-z]*\)/[^/]*/\$!\1$vault_baseurl  \2!" \
+  </etc/yum.repos.d/dist/CentOS-Base.repo \
+  >/etc/yum.repos.d/CentOS-Base.repo \
+;
 
 sed \
   -e "s/^\[/[C$el_ver_major-/" \
   -e '/^enabled=/d' \
   -e 's/^gpgcheck=.*/&\nenabled=0/' \
   -e 's!^mirrorlist=!#&!' \
-  -e "s!^#*\(baseurl=\).*!\1$latest_baseurl!" \
+  -e "s!^#*\(baseurl=\).*/\([a-z]*\)/[^/]*/\$!\1$latest_baseurl  \2!" \
   </etc/yum.repos.d/dist/CentOS-Base.repo \
   >/etc/yum.repos.d/CentOS-Base-Latest.repo \
-;
-sed \
-  -e 's!^mirrorlist=!#&!' \
-  -e "s!^#*\(baseurl=\).*!\1$vault_baseurl!" \
-  </etc/yum.repos.d/dist/CentOS-Base.repo \
-  >/etc/yum.repos.d/CentOS-Base.repo \
 ;
 
 mkdir -m 0755 -p /etc/yum/vars
