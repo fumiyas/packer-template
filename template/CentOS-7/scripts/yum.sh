a8922a1f46f2296e386068721fa4365acfe5191c
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2016-01-28 18:56:00 +0900

    CentOS: yum.sh: Add http://mirror.centos.org/centos/ to latet_baseurl

--- template/CentOS-7/scripts/yum.sh
+++ template/CentOS-7/scripts/yum.sh
@@ -9,7 +9,8 @@ el_ver_major="${el_ver%%.*}"
 latest_baseurl="\n\
   http://ftp.iij.ad.jp/pub/linux/centos/$el_ver_major/\\2/\$basearch/\n\
   http://ftp.kddlabs.co.jp/Linux/packages/CentOS/$el_ver_major/\\2/\$basearch/\n\
-  http://ftp.jaist.ac.jp/pub/Linux/CentOS/$el_ver_major/\\2/\$basearch/"
+  http://ftp.jaist.ac.jp/pub/Linux/CentOS/$el_ver_major/\\2/\$basearch/\n\
+  http://mirror.centos.org/centos/$el_ver_major/\\2/\$basearch/"
 vault_baseurl="\n\
   http://centos.data-hotel.net/pub/linux/centos/\$releasever/\\2/\$basearch/\n\
   http://vault.centos.org/\$releasever/\\2/\$basearch/\n\
