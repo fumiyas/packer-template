e483119f5e07ae05d57f495a349f68150a1d9a6d
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2018-02-13 20:22:55 +0900

    fix: CentOS 7: epel-release is avialable in extra repo

--- template/CentOS-7/scripts/epel.sh
+++ template/CentOS-7/scripts/epel.sh
@@ -6,24 +6,7 @@
 set -u
 set -e
 
-epel_url_prefix="https://download.fedoraproject.org/pub/epel"
-el_ver=$(sed 's/ *(.*//;s/.* //' /etc/redhat-release)
-el_ver_major="${el_ver%%.*}"
-el_arch=$(uname -m)
-
-epel_url="$epel_url_prefix/$el_ver_major/$el_arch/Packages"
-
-epel_release_rpm=$(
-  curl \
-    --location \
-    "$epel_url/e/" \
-  |sed \
-    -n \
-    's/.*<a href="\(epel-release-[0-9]\{1,\}-[0-9]\{1,\}\.noarch\.rpm\)".*/\1/p' \
-  ;
-)
-
-rpm -iv "$epel_url/e/$epel_release_rpm"
+yum install --assumeyes epel-release
 
 mkdir -p /etc/yum.repos.d/dist
 cp -a /etc/yum.repos.d/epel{,-*}.repo /etc/yum.repos.d/dist/
