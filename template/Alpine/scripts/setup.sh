ef0cdf817d12e112bb4f72a6f044deb21e311b2f
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2018-12-21 19:13:19 +0900

    feat: Add Alpine Linux template

---
+++ template/Alpine/scripts/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -u
+umask 0022
+
+## ======================================================================
+
+apk add sudo || exit $?
+
+## ======================================================================
+
+sed -i.dist \
+  -e 's/^#*\(PermitRootLogin\).*/\1 without-password/' \
+  -e 's/^#*\(UseDNS\).*/\1 no/' \
+  /etc/ssh/sshd_config \
+|| exit $? \
+;
+
+mkdir -p /etc/skel/.ssh || exit $?
+touch /etc/skel/.ssh/authorized_keys || exit $?
+
+## ======================================================================
+
+LC_TIME=C date >/etc/vagrant_box_build_time
+
+vagrant_home="/srv/vagrant"
+
+addgroup -S vagrant || exit $?
+adduser -S -D -G vagrant -h "$vagrant_home" vagrant || exit $?
+
+echo 'Defaults:vagrant !requiretty' >/etc/sudoers.d/vagrant || exit $?
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers.d/vagrant || exit $?
+
+mkdir -p "$vagrant_home/.ssh" || exit $?
+wget \
+  -O "$vagrant_home/.ssh/authorized_keys" \
+  https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub \
+|| exit $? \
+;
+chown -hR vagrant: "$vagrant_home/.ssh" || exit $?
+
+## ======================================================================
+
+exit 0
