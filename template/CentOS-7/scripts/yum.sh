6e703f1ec3d8ded875203ae0a7e909912616ba93
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2019-10-03 19:00:26 +0900

    refactor: C7: Use the original $fixedver to lock OS version
    instead of $releasever.

--- template/CentOS-7/scripts/yum.sh
+++ template/CentOS-7/scripts/yum.sh
@@ -4,40 +4,38 @@ set -u
 umask 0022
 
 el_ver=$(sed 's/ *(.*//;s/.* //' /etc/redhat-release)
-el_ver_major="${el_ver%%.*}"
 
 latest_baseurl="\n\
-  http://ftp.iij.ad.jp/pub/linux/centos/$el_ver_major/\\2/\$basearch/\n\
-  http://ftp.kddlabs.co.jp/Linux/packages/CentOS/$el_ver_major/\\2/\$basearch/\n\
-  http://ftp.jaist.ac.jp/pub/Linux/CentOS/$el_ver_major/\\2/\$basearch/\n\
-  http://mirror.centos.org/centos/$el_ver_major/\\2/\$basearch/"
-vault_baseurl="\n\
+  http://ftp.iij.ad.jp/pub/linux/centos/\$releasever/\\2/\$basearch/\n\
+  http://ftp.kddlabs.co.jp/Linux/packages/CentOS/\$releasever/\\2/\$basearch/\n\
   http://ftp.jaist.ac.jp/pub/Linux/CentOS/\$releasever/\\2/\$basearch/\n\
-  http://ftp.jaist.ac.jp/pub/Linux/CentOS-vault/\$releasever/\\2/\$basearch/\n\
-  http://mirror.centos.org/centos/\$releasever/\\2/\$basearch/\n\
-  http://vault.centos.org/\$releasever/\\2/\$basearch/\n\
-  http://archive.kernel.org/centos-vault/\$releasever/\\2/\$basearch/"
+  http://mirror.centos.org/centos/\$releasever/\\2/\$basearch/"
+fixed_baseurl="\n\
+  http://ftp.jaist.ac.jp/pub/Linux/CentOS/\$fixedver/\\2/\$basearch/\n\
+  http://ftp.jaist.ac.jp/pub/Linux/CentOS-vault/\$fixedver/\\2/\$basearch/\n\
+  http://mirror.centos.org/centos/\$fixedver/\\2/\$basearch/\n\
+  http://vault.centos.org/\$fixedver/\\2/\$basearch/\n\
+  http://archive.kernel.org/centos-vault/\$fixedver/\\2/\$basearch/"
+
+echo "$el_ver" >"/etc/yum/vars/fixedver"
 
 mkdir -p /etc/yum.repos.d/dist
 cp -a /etc/yum.repos.d/CentOS-*.repo /etc/yum.repos.d/dist
 
 sed \
+  -e "s/^\[/[FixedVer-/" \
+  -e 's!^\(name=.*\)\$releasever!\1$fixedver!' \
   -e 's!^mirrorlist=!#&!' \
-  -e "s!^#*\(baseurl=\).*/\([a-z]*\)/[^/]*/\$!\1$vault_baseurl!" \
+  -e "s!^#*\(baseurl=\).*/\([a-z]*\)/[^/]*/\$!\1$fixed_baseurl!" \
   </etc/yum.repos.d/dist/CentOS-Base.repo \
-  >/etc/yum.repos.d/CentOS-Base.repo \
+  >/etc/yum.repos.d/CentOS-Base-FixedVer.repo \
 ;
 
 sed \
-  -e "s/^\[/[C$el_ver_major-/" \
   -e '/^enabled=/d' \
   -e 's/^gpgcheck=.*/&\nenabled=0/' \
   -e 's!^mirrorlist=!#&!' \
   -e "s!^#*\(baseurl=\).*/\([a-z]*\)/[^/]*/\$!\1$latest_baseurl!" \
   </etc/yum.repos.d/dist/CentOS-Base.repo \
-  >/etc/yum.repos.d/CentOS-Base-Latest.repo \
+  >/etc/yum.repos.d/CentOS-Base.repo \
 ;
-
-mkdir -m 0755 -p /etc/yum/vars
-echo "$el_ver" >/etc/yum/vars/releasever
-
