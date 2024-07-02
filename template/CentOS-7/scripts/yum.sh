8064671ab97067a7aeee785964da2935a65ed71f
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-07-02 10:52:31 +0900

    bug: C7: Fix CentOS-vault repo URLs

--- template/CentOS-7/scripts/yum.sh
+++ template/CentOS-7/scripts/yum.sh
@@ -9,14 +9,14 @@ el_ver=$(sed 's/ *(.*//;s/.* //' /etc/redhat-release)
 latest_baseurl="\n\
   http://ftp.iij.ad.jp/pub/linux/centos/\$releasever/\\2/\$basearch/\n\
   http://ftp.jaist.ac.jp/pub/Linux/CentOS/\$releasever/\\2/\$basearch/\n\
-  https://vault.centos.org/\$releasever/\\2/\$basearch/\n\
+  https://vault.centos.org/centos/\$releasever/\\2/\$basearch/\n\
   https://archive.kernel.org/centos-vault/centos/\$releasever/\\2/\$basearch/"
 fixed_baseurl="\n\
   http://ftp.iij.ad.jp/pub/linux/centos/\$fixedver/\\2/\$basearch/\n\
   http://ftp.iij.ad.jp/pub/linux/centos-vault/centos/\$fixedver/\\2/\$basearch/\n\
   http://ftp.jaist.ac.jp/pub/Linux/CentOS/\$fixedver/\\2/\$basearch/\n\
   http://ftp.jaist.ac.jp/pub/Linux/CentOS-vault/centos/\$fixedver/\\2/\$basearch/\n\
-  https://vault.centos.org/\$fixedver/\\2/\$basearch/\n\
+  https://vault.centos.org/centos/\$fixedver/\\2/\$basearch/\n\
   https://archive.kernel.org/centos-vault/centos/\$fixedver/\\2/\$basearch/"
 
 echo "$el_ver" >"/etc/yum/vars/fixedver"
